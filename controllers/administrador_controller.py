98a6c6bfc26f2906dec633132ac7d539ad431947
CGUERRA96 <christian.guerra101013@gmail.com>
2020-09-28 22:38:39 -0500

    Actualización Antoni

--- controllers/administrador_controller.py
+++ controllers/administrador_controller.py
@@ -11,14 +11,17 @@ class Administrador_controller:
         self.prestamo_controlador = Prestamo_controller()
 
     def menu(self):
+        print('''
+        =============================
+            Bienvenido Administrador
+        =============================
+        ''')
+        self.identificar_administrador()
         while True:
             try:
-                print('''
-                =============================
-                   Bienvenido Administrador
-                =============================
-                ''')
-                self.identificar_administrador()
+                
+                print()
+
                 menu = ['Mostrar Administradores', "Mostrar Lectores", 'Inscribir personas', "Revisar solicitudes de préstamo", "Seguimiento de libros prestados", "Mantenimiento de estados de préstamos", "Salir"]
                 respuesta = Menu(menu).show()
                 
@@ -42,17 +45,78 @@ class Administrador_controller:
 
     def identificar_administrador(self):
         #buscas y seleccionas un administrador (filtro N° 1 en tipo_rol)
-        pass
+        self.listar_administradores()
+
+        id_admin = input_data("Ingrese el ID del profesor >> ", "int")
+        admin = self.persona.obtener_persona({'id_persona': id_admin})
+        #print(print_table(lector, ['id_persona', 'dni_persona', 'nombres', 'apellidos', 'correo', 'telefono', 'direccion', 'id_tipo_rol']))
+
+        print(f'''
+            ==================================================
+                Bienvenido Administrador : {admin[2]} {admin[3]}
+            ==================================================
+        ''')
+    
+    def listar_personas(self):
+        print('''
+        =======================
+        =  Lista de Personas  =
+        =======================
+        ''')
+        #personas_adm = self.persona.obtener_personas('id_persona')
+        persona = self.persona.obtener_personas('id_persona')
+        print(print_table(persona, ['id_persona', 'dni_persona', 'nombres', 'apellidos', 'correo', 'telefono', 'direccion', 'id_tipo_rol']))
+        input("\nPresione una tecla para continuar...")
 
     def listar_administradores(self):
         #Buscar administradores (filtro administrador rol)
-        pass
+        print('''
+        ==============================
+        =  Lista de Administradores  =
+        ==============================
+        ''')
+        id_tipo_rol = 1
+        personas_adm = self.persona.buscar_personas({'id_tipo_rol': id_tipo_rol})
+        print(print_table(personas_adm, ['id_persona', 'dni_persona', 'nombres', 'apellidos', 'correo', 'telefono', 'direccion', 'id_tipo_rol']))
+        input("\nPresione una tecla para continuar...")
 
     def listar_lectores(self):
         #Buscar lectores (filtro lector rol)
-        pass
+        print('''
+        =======================
+        =  Lista de Lectores  =
+        =======================
+        ''')
+        id_tipo_rol = 2
+        personas_lec = self.persona.buscar_personas({'id_tipo_rol': id_tipo_rol})
+        print(print_table(personas_lec, ['id_persona', 'dni_persona', 'nombres', 'apellidos', 'correo', 'telefono', 'direccion', 'id_tipo_rol']))
+        input("\nPresione una tecla para continuar...")
 
     def inscribir_personas(self):
         #Ingresa persona y coloca su rol (1: Administrador y 2: Lector)
-        pass
+        dni = input_data("Ingrese el nuevo DNI >> ")
+        nombre = input_data("Ingrese el nuevo nombre >> ")
+        apellidos = input_data("Ingrese el nuevo apellido >> ")
+        correo = input_data("Ingrese el nuevo correo >> ")
+        telefono = input_data("Ingrese el nuevo telefono >> ")
+        direccion = input_data("Ingrese la nueva direccion >> ")
+        id_tipo_rol = input_data("Ingrese el ID rol >> ", "int")
+        self.persona.guardar_persona({
+            'dni_persona ': dni,
+            'nombres': nombre,
+            'apellidos': apellidos,
+            'correo': correo,
+            'telefono': telefono,
+            'direccion': direccion,
+            'id_tipo_rol': id_tipo_rol
+        })
+        print('''
+        ==========================
+            Persona Agregada !
+        ==========================
+        ''')
+
+        self.listar_personas()
+
+
 
--- controllers/lector_controller.py
+++ controllers/lector_controller.py
@@ -1,6 +1,7 @@
 from classes.personas import Persona
 from controllers.libros_controller import Libro_controller
 from controllers.prestamos_controller import Prestamo_controller
+from controllers.administrador_controller import Administrador_controller
 from helpers.helper import input_data, print_table, pregunta
 from helpers.menu import Menu
 
@@ -8,17 +9,19 @@ class Lector_controller:
     def __init__(self):
         self.persona = Persona()        
         self.libro_controlador = Libro_controller()
+        self.administrador_controller = Administrador_controller()
         self.prestamo_controlador = Prestamo_controller()
 
     def menu(self):
+        print('''
+        ========================
+            Bienvenido Lector
+        ========================
+        ''')
+        self.identificar_lector()
         while True:
             try:
-                print('''
-                ========================
-                   Bienvenido Lector
-                ========================
-                ''')
-                self.identificar_lector()
+                print()
                 menu = ['Buscar libros', "Solicitar libro", "Historial de solicitudes","Salir"]
                 respuesta = Menu(menu).show()
                 if respuesta == 1:
@@ -35,5 +38,15 @@ class Lector_controller:
 
     def identificar_lector(self):
         # Buscar e identificar al lector (filtro N° 2 en la tabla personas)
-        pass
+        self.administrador_controller.listar_lectores()
+
+        id_lector = input_data("Ingrese el ID del profesor >> ", "int")
+        lector = self.persona.obtener_persona({'id_persona': id_lector})
+        #print(print_table(lector, ['id_persona', 'dni_persona', 'nombres', 'apellidos', 'correo', 'telefono', 'direccion', 'id_tipo_rol']))
+
+        print(f'''
+            ==============================================
+                Bienvenido Lector : {lector[2]} {lector[3]}
+            ==============================================
+        ''')
     
